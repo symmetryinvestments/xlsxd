bdce55282388c8a52a13f3fded9a1264a00e3f0a
Robert Schadek <rschadek@symmetryinvestments.com>
2019-01-21 14:09:02 +0000

    less rebuilding of stuff

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 source/libxlsxd/xlsxwrap.d: source/libxlsxd/xlsxwrap.dpp
 	d++ --include-path libxlsxwriter/include/ --keep-d-files --preprocess-only source/libxlsxd/xlsxwrap.dpp
 
-buildclib:
+libxlsxwriter/build/libxlsxwriter.a:
 	cd libxlsxwriter/ && mkdir -p build && cd build && cmake .. && make -j6
 
--- windepbuild.bat
+++ windepbuild.bat
@@ -5,23 +5,31 @@ set ZLIB_PATH=%~dp0\zlib\build
 cd %WORK_DIR%
 
 Rem git clone https://github.com/madler/zlib.git
-cd zlib
-mkdir build
-cd    build
-echo %~dp0
-cmake .. -DCMAKE_INSTALL_PREFIX:STRING="%INSTALL_DIR%\zlib" -DCMAKE_GENERATOR_PLATFORM=x64
+if not exist "\install_dir\zlib\lib\zlibstatic.lib" (
+	cd zlib
+	mkdir build
+	cd    build
+	echo %~dp0
+	cmake .. -DCMAKE_INSTALL_PREFIX:STRING="%INSTALL_DIR%\zlib" -DCMAKE_GENERATOR_PLATFORM=x64
 
-cmake --build . --config Release  --target install
+	cmake --build . --config Release  --target install
 
-cd %WORK_DIR%
+	cd %WORK_DIR%
+) else (
+	Rem "zlib already build"
+)
 
 Rem git clone https://github.com/jmcnamara/libxlsxwriter.git
-cd libxlsxwriter
-mkdir build
-cd    build
-echo %~dp0
-cmake .. -DCMAKE_INSTALL_PREFIX:STRING="%INSTALL_DIR%\libxlsxwriter" -DZLIB_ROOT:STRING="%INSTALL_DIR%\zlib" -DCMAKE_GENERATOR_PLATFORM=x64
+if not exist "\install_dir\libxlsxwriter\lib\x64\Release\xlsxwriter.lib" (
+	cd libxlsxwriter
+	mkdir build
+	cd    build
+	echo %~dp0
+	cmake .. -DCMAKE_INSTALL_PREFIX:STRING="%INSTALL_DIR%\libxlsxwriter" -DZLIB_ROOT:STRING="%INSTALL_DIR%\zlib" -DCMAKE_GENERATOR_PLATFORM=x64
 
-cmake --build . --config Release --target install
+	cmake --build . --config Release --target install
 
-cd %WORK_DIR%
+	cd %WORK_DIR%
+) else (
+	Rem "libxlsxwriter already build"
+)
