a9e468bc2fc36b823a732a4ebbb239929098d3d1
John Colvin <john.loughran.colvin@gmail.com>
2019-08-20 14:54:52 +0100

    safer batch file

--- windepbuild.bat
+++ windepbuild.bat
@@ -1,8 +1,11 @@
 @echo off
+setlocal EnableDelayedExpansion
+
 set    WORK_DIR=%~dp0
 set INSTALL_DIR=%~dp0\install_dir
 
-cd %WORK_DIR%
+pushd "%WORK_DIR%"
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 Rem git clone https://github.com/madler/zlib.git
 
@@ -11,14 +14,20 @@ if exist "%INSTALL_DIR%\zlib\lib\zlibstatic.lib" (
 )  else (
 	echo "zlib does not exist"
 	cd zlib
+	if !errorlevel! neq 0 exit /b !errorlevel!
 	mkdir build
+	if !errorlevel! neq 0 exit /b !errorlevel!
 	cd    build
+	if !errorlevel! neq 0 exit /b !errorlevel!
 	echo %~dp0
 	cmake .. -DCMAKE_INSTALL_PREFIX:STRING="%INSTALL_DIR%\zlib" -DCMAKE_GENERATOR_PLATFORM=x64
+	if !errorlevel! neq 0 exit /b !errorlevel!
 
 	cmake --build . --config Release  --target install
+	if !errorlevel! neq 0 exit /b !errorlevel!
 
-	cd %WORK_DIR%
+	cd "%WORK_DIR%"
+	if !errorlevel! neq 0 exit /b !errorlevel!
 )
 
 Rem git clone https://github.com/jmcnamara/libxlsxwriter.git
@@ -28,12 +37,18 @@ if exist "%INSTALL_DIR%\libxlsxwriter\lib\x64\Release\xlsxwriter.lib" (
 ) else (
 	echo "libxlsxwriter does not exist"
 	cd libxlsxwriter
+	if !errorlevel! neq 0 exit /b !errorlevel!
 	mkdir build
+	if !errorlevel! neq 0 exit /b !errorlevel!
 	cd    build
+	if !errorlevel! neq 0 exit /b !errorlevel!
 	echo %~dp0
 	cmake .. -DCMAKE_INSTALL_PREFIX:STRING="%INSTALL_DIR%\libxlsxwriter" -DZLIB_ROOT:STRING="%INSTALL_DIR%\zlib" -DCMAKE_GENERATOR_PLATFORM=x64
+	if !errorlevel! neq 0 exit /b !errorlevel!
 
 	cmake --build . --config Release --target install
+	if !errorlevel! neq 0 exit /b !errorlevel!
 
-	cd %WORK_DIR%
+	cd "%WORK_DIR%"
+	if !errorlevel! neq 0 exit /b !errorlevel!
 )
