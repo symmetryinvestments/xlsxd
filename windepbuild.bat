d811ef5df71ebd500b3d7d1b1b457095d4c9b21f
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2021-07-08 11:21:47 +0200

    Windows: Build 32/64-bit libxlsxwriter selectively
    This saves about 7 seconds build time in my Threadripper VM.

--- windepbuild.bat
+++ windepbuild.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+set ARCH=%1
+
 set    WORK_DIR=%~dp0
 set INSTALL_DIR=%~dp0\install_dir
 
@@ -8,44 +10,19 @@ cd /d "%WORK_DIR%"
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 Rem git clone https://github.com/jmcnamara/libxlsxwriter.git
-Rem x86
-if exist "%INSTALL_DIR%\libxlsxwriter\lib\Win32\Release\xlsxwriter.lib" (
-	echo 32-bit libxlsxwriter already built
+if exist "%INSTALL_DIR%\libxlsxwriter\lib\%ARCH%\Release\xlsxwriter.lib" (
+	echo %ARCH% libxlsxwriter already built
 ) else (
-	echo 32-bit libxlsxwriter does not exist
+	echo %ARCH% libxlsxwriter does not exist
 	cd libxlsxwriter
 	if !errorlevel! neq 0 exit /b !errorlevel!
-	mkdir build86
+	mkdir build_%ARCH%
 	if !errorlevel! neq 0 exit /b !errorlevel!
-	cd    build86
+	cd    build_%ARCH%
 	if !errorlevel! neq 0 exit /b !errorlevel!
-	cmake .. -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%\libxlsxwriter" -DCMAKE_GENERATOR_PLATFORM=Win32
+	cmake .. -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%\libxlsxwriter" -DCMAKE_GENERATOR_PLATFORM=%ARCH%
 	if !errorlevel! neq 0 exit /b !errorlevel!
 
 	cmake --build . --config Release --target install
-	if !errorlevel! neq 0 exit /b !errorlevel!
-
-	cd "%WORK_DIR%"
-	if !errorlevel! neq 0 exit /b !errorlevel!
-)
-
-Rem x64
-if exist "%INSTALL_DIR%\libxlsxwriter\lib\x64\Release\xlsxwriter.lib" (
-	echo 64-bit libxlsxwriter already built
-) else (
-	echo 64-bit libxlsxwriter does not exist
-	cd libxlsxwriter
-	if !errorlevel! neq 0 exit /b !errorlevel!
-	mkdir build
-	if !errorlevel! neq 0 exit /b !errorlevel!
-	cd    build
-	if !errorlevel! neq 0 exit /b !errorlevel!
-	cmake .. -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%\libxlsxwriter" -DCMAKE_GENERATOR_PLATFORM=x64
-	if !errorlevel! neq 0 exit /b !errorlevel!
-
-	cmake --build . --config Release --target install
-	if !errorlevel! neq 0 exit /b !errorlevel!
-
-	cd "%WORK_DIR%"
 	if !errorlevel! neq 0 exit /b !errorlevel!
 )
